c8c4c9c186570806aac85d0c1c0392c05158832f
faizrizqiadhityaputra <faizrizqiadhityaputra@mail.ugm.ac.id>
2026-07-20 12:43:00 +0700

    lastt pls

--- api/index.php
+++ api/index.php
@@ -1,8 +1,3 @@
 <?php
 
-// --- TAMBAHKAN 2 BARIS INI UNTUK MEMAKSA HAPUS CACHE DI VERCEL ---
-array_map('unlink', glob(__DIR__ . '/../bootstrap/cache/*.php'));
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-// -----------------------------------------------------------------
-
 require __DIR__ . '/../public/index.php';
